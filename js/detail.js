a70ffa0bf3610a3e3bd702c93c923a323893c91a
d12889811 <1216985248@qq.com>
2023-10-16 01:19:48 -0400

    update1
    carousel&star rate

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,29 @@
+const allElements = document.body.getElementsByTagName('*');
+for (let element of allElements) {
+    element.classList.add('Detail0');
+}
+
 var screenWidth = screen.width;
 var detailbody=document.querySelector(".detail_main_container");
 detailbody.style.setProperty('--screenwith',screenWidth+'px');
+
+stars=document.querySelectorAll(".detail_rating span")
+stars.forEach(function (element) {
+    element.addEventListener('mouseover',function (event) {
+        var score=document.querySelector(".detail_score h2")
+        var value=event.target.getAttribute('data-value');
+        console.log(value);
+        for (var i=1; i<=5;i++){
+            if (i<=value){
+                stars[i-1].textContent='★';
+                stars[i-1].classList.add('active');
+            }
+            else{
+                stars[i-1].textContent='☆';
+                stars[i-1].classList.remove('active');
+            }
+        }
+        score.innerHTML="<span class='detail_yellow_bold'>"+value+".0"+"</span><span style='font-size: 20px'>/5.0</span>";
+    })
+})
+
