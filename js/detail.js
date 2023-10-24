5b607e3c4c6e28e1d3cb8f82244fca8cc8b25f75
d12889811 <1216985248@qq.com>
2023-10-24 16:21:51 -0400

    1

--- js/detail.js
+++ js/detail.js
@@ -6,11 +6,12 @@ for (let element of allElements) {
 var screenWidth = screen.width;
 var detailbody=document.querySelector(".detail_main_container");
 //detailbody.style.setProperty('--screenwith',screenWidth+'px');
-
+var starvalue;
 stars=document.querySelectorAll(".detail_input_start_container span")
 stars.forEach(function (element) {
     element.addEventListener('mouseover',function (event) {
         var value=event.target.getAttribute('data-value');
+        starvalue=value;
         console.log(value);
         for (var i=1; i<=5;i++){
             if (i<=value){
@@ -30,7 +31,31 @@ stars.forEach(function (element) {
 // check if the user has logged in
 // if not, redirect to login page
 // if yes, submit the comment
-var user_info = window.localStorage.getItem("user");
+function getTime(){
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;  // 月份是从0开始的，所以+1
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return formattedDate;
+}
+function submitcomment(){
+    var time=getTime();
+    var st_rating=starvalue.toString();
+    var name="";
+    var email="";
+    var avatar="";
+    let comments = JSON.parse(localStorage.getItem("comments"));
+    var id=(comments.length+1).toString();
+    if(window.localStorage.getItem("user")){
+        var user=JSON.parse(localStorage.getItem("user"))
+        name=user.user;
+
+    }
+}
 var new_review = {
     "review_id": "9",
     "reviewer_name": user_info.user_name,
